d52ca055e953ccef97037f7c4eaa79e99a82dbef
Alex Levine <aalexlevine@gmail.com>
2017-09-22 20:33:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,7 @@
+
 function forLoop(array) {
- for (let i = 0; i < 25; i++){
-   if (i === 1) {console.log{"I am 1 strange loop."}
+ for (let i = 0; i < 25; i++) {
+  if (i === 1) {array.push("I am 1 strange loop.")}
+  else {array.push("I am ${i} strange loops.")}
  }
-  else console.log("I am" + ${i} + "strange loops.")
-}
-
-return array
-}
-
+ return array;
