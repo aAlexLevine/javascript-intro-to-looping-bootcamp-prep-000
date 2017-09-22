f74c470f119d0e45534164e7173f4f5986478fb2
Alex Levine <aalexlevine@gmail.com>
2017-09-22 20:13:03 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,22 @@
+/*for ([initialization]; [condition]; [iteration]) {
+  [loopBody]
+}
+
+var gifts = ['teddy bear', 'drone', 'doll']
+
+function wrapGift(gift) {
+  console.log(`Wrapped ${gift} and added a bow!`)
+
+  for (var i = 1; i < 100; i++) {
+    console.log( "Hello World the " + i + " time" );
+
+
+*/
+
+function forLoop(array) {
+ for (let i = 0; i < 25; i++)
+    console.log("I am" + ${i} "strange loops.")
+       (let i = 1;
+         console.log("I am" + i + "strange loop.")
+return array
+}
